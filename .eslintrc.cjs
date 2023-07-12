01f1b755fb0a263810f851b597de6f4db20492b3
xooxlight <xooxlight@gmail.com>
2023-07-13 08:19:47 +0900

    Setting ESLint, Prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,32 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
   ],
+  env: {
+    'vue/setup-compiler-macros': true,
+  },
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+        tabWidth: 2,
+        trailingComma: 'es5',
+        printWidth: 80,
+        bracketSpacing: true,
+        arrowParens: 'always',
+        endOfLine: 'auto',
+      },
+    ],
+  },
+};
