31887bd839af017f2fc2e1a5a1c751d785801bc4
xooxlight <xooxlight@gmail.com>
2023-07-13 08:30:37 +0900

    Install Axios

--- src/main.js
+++ src/main.js
@@ -5,10 +5,14 @@ import { createPinia } from 'pinia';
 
 import App from './App.vue';
 import router from './router';
+import axios from 'axios';
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+
+app.provide('$axios', axios);
+app.config.globalProperties.$axios = axios;
 
 app.mount('#app');
